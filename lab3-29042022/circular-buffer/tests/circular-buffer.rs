931c0884476d87f49967311eaa751dd89b69f68f
Simone Cosimo <simosx4@gmail.com>
2022-04-30 21:36:27 +0200

    Added tests for 2nd exercise: MyCycle iterator

--- lab3-29042022/circular-buffer/tests/circular-buffer.rs
+++ lab3-29042022/circular-buffer/tests/circular-buffer.rs
@@ -1,4 +1,4 @@
-use circular_buffer::{CircularBuffer, Error};
+use circular_buffer::{CircularBuffer, MyCycle, Error};
 use std::rc::Rc;
 
 #[test]
@@ -162,4 +162,43 @@ fn string_buffer() {
     buffer.write("Testing".to_string()).unwrap();
     assert_eq!(0, buffer.read().unwrap().len());
     assert_eq!(Ok("Testing".to_string()), buffer.read());
+}
+
+#[test]
+#[ignore]
+fn empty_iterator() {
+    let empty = Vec::<i32>::new();
+    let cycle= MyCycle::new(empty.iter(), 4);
+    assert_eq!(0, cycle.count());
+}
+
+#[test]
+#[ignore]
+fn recursive_cycle() {
+    let v = vec![1, 2, 3, 4];
+    let cycle1 = MyCycle::new(v.iter(), 4);
+    let cycle2 = MyCycle::new(cycle1, 2);
+    assert_eq!((v.len() * 4) * 2, cycle2.count());
+}
+
+#[test]
+#[ignore]
+fn chained_cycles() {
+    let v1 = vec![1, 2, 3, 4];
+    let v2 = vec![1, 2, 3, 4, 5, 6, 7];
+    let cycle1 = MyCycle::new(v1.iter(), 4);
+    let cycle2 = MyCycle::new(v2.iter(), 2);
+    assert_eq!(v1.len() * 4 + v2.len() * 2, cycle2.chain(cycle1).count());
+}
+
+#[test]
+#[ignore]
+fn zip_cycles() {
+    let v1 = vec![1, 3, 5, 7];
+    let v2 = vec![2, 4, 6, 8];
+    let cycle1 = MyCycle::new(v1.iter(), 2);
+    let cycle2 = MyCycle::new(v2.iter(), 2);
+    let mut res= Vec::new();
+    for (x, y) in cycle1.zip(cycle2) { res.push(*x); res.push(*y); }
+    assert_eq!(vec![1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8], res);
 }
